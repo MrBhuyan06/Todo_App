994139ecace424c461017ea185824f415e6170c9
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-09-22 13:55:15 +0530

    render the list of task from the api

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { TiTickOutline, TiCancel, TiDelete, TiEdit } from "react-icons/ti";
 
-const Todo = () => {
+const Todo = ({ info }) => {
+  console.log(info);
+  const { title, completed } = info;
   return (
-    <div className="flex items-center gap-4 justify-center w-4/5   border-2  mx-auto ">
-      <TiTickOutline className="text-2xl" />
+    <div className="flex items-center gap-4 justify-center w-4/5   border-2  mx-auto mb-2 ">
+      <TiTickOutline
+        className={`text-2xl cursor-pointer ${
+          completed ? "text-red-300" : "text-blue-400"
+        }  `}
+      />
       <input
         type="text"
         className="w-2/3 p-2 rounded-md outline-none bg-transparent"
         name=""
+        value={title}
         id=""
       />
-      <TiDelete className="text-2xl" />
-      <TiEdit className="text-2xl" />
+      <TiDelete className="text-2xl cursor-pointer" />
+      <TiEdit className="text-2xl cursor-pointer" />
     </div>
   );
 };
--- src/components/TodoBody.js
+++ src/components/TodoBody.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Filter from "./Filter.js";
 import Todo from "./Todo.js";
 
 const TodoBody = () => {
+  const [listTask, setListTask] = useState([]);
+  console.log("heelo");
+
+  useEffect(() => {
+    getAllTodo();
+  }, []);
+
+  async function getAllTodo() {
+    const streamData = await fetch(
+      "https://jsonplaceholder.typicode.com/users/1/todos"
+    );
+    const data = await streamData.json();
+
+    setListTask(data);
+  }
+
   return (
     <div className="bg-gray-100 rounded-md mt-10 w-2/3 mx-auto">
       <p className="text-center text-3xl underline text-blue-600 font-semibold">
         List Of Task
       </p>
       <Filter />
-      <Todo />
+      {listTask.length === 0 ? (
+        <h1>Add The Task Please</h1>
+      ) : (
+        listTask.map((task) => {
+          return <Todo key={task.id} info={task} />;
+        })
+      )}
     </div>
   );
 };
